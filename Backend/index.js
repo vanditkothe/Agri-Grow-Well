ebbca8e9874e7ebacb854d64ee57aa4ad57e1d68
vandit kothe <vanditkothe@vandits-MacBook-Air.local>
2026-04-24 17:45:17 +0530

    fix: retry logic added3

--- Backend/index.js
+++ Backend/index.js
@@ -45,7 +45,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 // }
 async function analyzeHealthWithGemini(prompt) {
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.5-flash-8b",
   });
 
   let retries = 3;
